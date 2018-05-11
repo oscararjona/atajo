8dbbc2826b4f41b39eb616137a0f7f3c0e567fd5
oscar arjona <oscar@DESKTOP-373J9G9>
2018-05-11 15:51:15 +0200

    correigido

--- RutaMasCorta/src/gps/Bicicleta.java
+++ RutaMasCorta/src/gps/Bicicleta.java
@@ -22,10 +22,18 @@ public class Bicicleta {
         for(int i=0; i<camino1.length; i++)
         {
             sum1 += camino1[i];
+            
+        }
+        for(int i=0; i<camino2.length; i++)
+        {
+         
             sum2 += camino2[i];
+            
+        }  for(int i=0; i<camino3.length; i++)
+        {
+            
             sum3 += camino3[i];
         }
-        
         if(sum1 < sum2 && sum1 < sum3)
         {
             return "camino 1"; 
